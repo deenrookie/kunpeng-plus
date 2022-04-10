1290436b8ab5449398db8b8c806df27687063be3
deenrookie <deenrookie@gmail.com>
2022-04-10 14:36:49 +0800

    Create vmware-954.go

---
+++ pocs/mypocs/vmware-954.go
@@ -0,0 +1,60 @@
+package mypocs
+
+import (
+	plugin "github.com/deenrookie/kunpeng-plus/pocs"
+	util "github.com/deenrookie/kunpeng-plus/utils"
+	"net/http"
+	"strings"
+)
+
+type vmware954RCE struct {
+	info   plugin.Plugin
+	result []plugin.Plugin
+}
+
+func init() {
+	plugin.Regist("vmware", &vmware954RCE{})
+}
+
+func (d *vmware954RCE) Init() plugin.Plugin {
+	d.info = plugin.Plugin{
+		Name:    "vmware",
+		Remarks: "vmware rce",
+		Level:   0,
+		Type:    "RCE",
+		Author:  "Deen",
+		References: plugin.References{
+			URL:  "",
+			CVE:  "",
+			KPID: "KP-1006",
+		},
+	}
+	return d.info
+}
+
+func (d *vmware954RCE) GetResult() []plugin.Plugin {
+	var result = d.result
+	d.result = []plugin.Plugin{}
+	return result
+}
+
+func (d *vmware954RCE) Check(URL string, meta plugin.TaskMeta) bool {
+	_ = meta
+
+	domain := util.GetHostFromUrl(URL)
+	if domain == "" {
+		return false
+	}
+	var request *http.Request
+
+	request, _ = http.NewRequest("GET", URL+"/catalog-portal/ui/oauth/verify", nil)
+	resp, err := util.RequestDo(request, false)
+	if &resp != nil && err == nil {
+		if resp.Other != nil {
+			if strings.Contains(string(resp.Body), "server.unexpected.error") {
+				return true
+			}
+		}
+	}
+	return false
+}
